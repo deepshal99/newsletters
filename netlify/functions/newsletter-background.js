d1ed4c9639960c2df8bdbd299277daa692c1635e
deepshal99 <deepshal99@gmail.com>
2025-04-10 19:11:53 +0000

    Fix: Change newsletter time to 12:48 AM IST

--- netlify/functions/newsletter-background.js
+++ netlify/functions/newsletter-background.js
@@ -1,7 +1,7 @@
 import { schedule } from '@netlify/functions';
 import { sendDailyNewsletter } from './newsletter.js';
 
-// Schedule the function to run daily at 17:35
+// Schedule the function to run daily at 12:48 AM IST (19:18 UTC)
 export const handler = schedule("35 17 * * *", async (event) => {
     try {
         console.log('Scheduled function triggered');
